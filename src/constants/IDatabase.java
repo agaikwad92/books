b2161169326aa4e881df710c9a5c4b864be5ac32
agaikwad92 <ajaygaikwad92@gmail.com>
2020-03-26 10:53:09 +0530

    update mysql

--- src/constants/IDatabase.java
+++ src/constants/IDatabase.java
@@ -3,7 +3,7 @@ package constants;
 public class IDatabase {
 
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	public static final String CONNECTION_STRING = "jdbc:mysql://localhost/onlinebookstore";
+	public static final String CONNECTION_STRING = "jdbc:mysql://db:3306/onlinebookstore";
 
 	public static final String USER_NAME = "aj";
 	public static final String PASSWORD = "a";
